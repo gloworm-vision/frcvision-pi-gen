e406b27f18e86f7e80bb5517afb68a10c2521b13
Hugo Hromic <hhromic@users.noreply.github.com>
2019-07-30 12:38:26 +0100

    Ensure that the configuration file is an absolute path in Docker build (#306)
    * Use `&&` instead of `;` in Docker pipeline
    
    * In case of error, `&&` does not continue execution
    
    * Silence shellcheck warning
    
    * SC2086: Double quote to prevent globbing and word splitting.
    
    * Ensure that the configuration file is an absolute path in Docker build
    
    The specific problem is in commit 2ddd7c1, where the passed config file
    (using the `-c` option) is now mounted inside the container using the
    `--volume src:dest:opt` Docker option.
    
    The problem is that Docker requires absolute paths for mounting single
    files inside the container, otherwise it silently tries to mount a volume
    name instead as an empty directory. Therefore the Docker build no longer
    works with the following invocation forms (relative config-paths):
    
        ./build-docker.sh -c myconfig
        /path/to/build-docker.sh -c myconfig   # also doesn't work
    
    This commit uses `realpath` (included in coreutils) in the Docker build
    script to ensure that the passed configuration file is always an
    absolute path before passing it to Docker.

--- build-docker.sh
+++ build-docker.sh
@@ -32,6 +32,9 @@ done
 
 IMG_VERSION=${IMG_VERSION:-$(git describe)}
 
+# Ensure that the configuration file is an absolute path
+CONFIG_FILE=$(realpath -s "$CONFIG_FILE")
+
 # Ensure that the confguration file is present
 if test -z "${CONFIG_FILE}"; then
 	echo "Configuration file need to be present in '${DIR}/config' or path passed as parameter"
@@ -76,7 +79,7 @@ if [ "${CONTAINER_EXISTS}" != "" ] && [ "${CONTINUE}" != "1" ]; then
 fi
 
 # Modify original build-options to allow config file to be mounted in the docker container
-BUILD_OPTS="$(echo ${BUILD_OPTS:-} | sed -E 's@\-c\s?([^ ]+)@-c /config@')"
+BUILD_OPTS="$(echo "${BUILD_OPTS:-}" | sed -E 's@\-c\s?([^ ]+)@-c /config@')"
 
 ${DOCKER} build -t pi-gen "${DIR}"
 if [ "${CONTAINER_EXISTS}" != "" ]; then
@@ -88,7 +91,7 @@ if [ "${CONTAINER_EXISTS}" != "" ]; then
 		-e IMG_VERSION="${IMG_VERSION}"\
 		pi-gen \
 		bash -e -o pipefail -c "dpkg-reconfigure qemu-user-static &&
-	cd /pi-gen; ./build.sh ${BUILD_OPTS} ;
+	cd /pi-gen; ./build.sh ${BUILD_OPTS} &&
 	rsync -av work/*/build.log deploy/" &
 	wait "$!"
 else
