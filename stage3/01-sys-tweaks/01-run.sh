f0399a216c5cc9571a8a743aa40aaa5fab038c55
Peter Johnson <johnson.peter@gmail.com>
2019-12-31 01:12:43 -0600

    Update WPILib to 2020.1.1 (#146)

--- stage3/01-sys-tweaks/01-run.sh
+++ stage3/01-sys-tweaks/01-run.sh
@@ -50,7 +50,7 @@ wget -nc -nv \
 
 # allwpilib
 wget -nc -nv -O allwpilib.tar.gz \
-    https://github.com/wpilibsuite/allwpilib/archive/1c1e0c9a6a896355931eb1385257a2c23e63efd4.tar.gz
+    https://github.com/wpilibsuite/allwpilib/archive/v2020.1.1.tar.gz
 
 # pynetworktables
 wget -nc -nv -O pynetworktables.tar.gz \
